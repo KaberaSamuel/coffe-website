e1063faaf73dbd5fb5ba401dac75dd83fb2051b7
KaberaSamuel <kaberanshutis@gmail.com>
2024-09-15 03:51:11 +0200

    using javascript to add animation of slide in texts

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const menu = document.querySelector("#menu");
 const closebutton = document.querySelector("#close");
 const aside = document.querySelector("aside");
 const body = document.querySelector("body");
+const home = document.querySelector("#home");
+const backToTop = document.querySelector("#back-to-top");
+
 let animateUpItems = Array.from(document.querySelectorAll(".animate-up"));
 const sideAnchorTags = Array.from(document.querySelectorAll("aside a"));
 let time = 0;
@@ -16,14 +19,42 @@ const array3 = Array.from(
   document.querySelectorAll("#services > div:last-child > *")
 );
 
+function handleHomeIntersection(entries) {
+  if (entries[0].isIntersecting) {
+    backToTop.style.opacity = 0;
+  } else {
+    backToTop.style.opacity = 1;
+  }
+}
+
+function handleFadeInIntersection(entries) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) entry.target.classList.add("show");
+  });
+}
+
+const homeObserver = new IntersectionObserver(handleHomeIntersection, {
+  threshold: 0.2,
+});
+
+homeObserver.observe(home);
+
 fadeInElements = fadeInElements.concat(array2, array3);
+
 function hideSidebar(event) {
   aside.classList.remove("show");
 }
+
 fadeInElements.push(document.querySelector("#services > h2"));
-fadeInElements.push(document.querySelector("iframe"));
+// fadeInElements.push(document.querySelector("iframe"));
 
 animateUpItems = animateUpItems.slice(3).concat(animateUpItems.slice(0, 3));
+
+const fadeInObserver = new IntersectionObserver(handleFadeInIntersection, {
+  threshold: 0,
+});
+
+fadeInElements.forEach((element) => fadeInObserver.observe(element));
 
 function showSidebar() {
   aside.classList.add("show");
